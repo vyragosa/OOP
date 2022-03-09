da4814984293e141e1dcb8ad2941aaff7cbee1f3
sad <39193849+vyragosa@users.noreply.github.com>
2022-03-09 13:35:25 +0300

    small fix

--- Ex 3_1_3/Ex 3_1_3/Ex 3_1_3.cpp
+++ Ex 3_1_3/Ex 3_1_3/Ex 3_1_3.cpp
@@ -7,17 +7,32 @@ int main()
 	int n, numb1, numb2;
 	cin >> name >> n;
 	Stack obj1(name, n);
-	cout << obj1.name << " " << obj1.n << endl;
+
 	cin >> name >> n;
 	Stack obj2(name, n);
-	cout << obj2.name << " " << obj2.n << endl;
+
 	do
 	{
 		cin >> numb1;
+
 	} while (obj1.fill(numb1) && obj2.fill(numb1));
-	cout << setw(15) << obj1.name << obj2.name;
-	while (obj1.show(numb1) && obj2.show(numb2))
+	cout << obj1.name << " " << obj1.n << endl;
+	cout << obj2.name << " " << obj2.n << endl;
+	cout << left << setw(15) << obj1.name 
+		<< left << obj2.name;
+	numb2 = obj1.size;
+	for (int i = 0; i < numb2; i++)
 	{
-		cout << left << right << setw(15) << numb1 << right << setw(15) << numb2;
+		cout << endl;
+		if (obj1.show(numb1))
+		{
+			cout << right << setw(15) << numb1;
+		}
+
+		if (obj2.show(numb1))
+		{
+			cout << right << setw(15) << numb1;
+		}
 	}
+
 }

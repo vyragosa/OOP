434abf898c79c212e6753fc136553244e916b31e
uumlaut <39193849+vyragosa@users.noreply.github.com>
2022-04-26 23:01:42 +0300

    Added 1_2_4

---
+++ Ex 1_2_4/Ex 1_2_4/Ex 1_2_4.cpp
@@ -0,0 +1,15 @@
+﻿#include "Expression.h"
+int main()
+{
+	int x, y;
+	char sign;
+	Expression obj;
+	cin >> x >> sign >> y;
+	obj.solution1(x, sign, y);
+	cin >> sign >> y;
+	obj.solution2(sign, y);
+	cin >> sign >> y;
+	obj.solution2(sign, y);
+	obj.result();
+	return 0;
+}

70baea537e1129097bfeb55bcaa79e1695b1ce39
uumlaut <39193849+vyragosa@users.noreply.github.com>
2022-04-26 21:20:49 +0300

    Added 1_1_3

---
+++ Ex 1_1_3/Ex 1_1_3/Ex 1_1_3.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main() {
+	int num, digits = 0;
+	std::cin >> num;
+	if (num == 0)
+		digits += 1;
+	while (num) {
+		digits += 1;
+		num /= 10;
+	}
+	std::cout << "Number of digits: " << digits;
+}

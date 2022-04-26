b7fe77e79573c2ed1f569452d8bf616149d5e772
sad <39193849+vyragosa@users.noreply.github.com>
2022-04-26 21:21:30 +0300

    Added 1_1_4

---
+++ Ex 1_1_4/Ex 1_1_4/Ex 1_1_4.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main() {
+	char symbol;
+	std::cin >> symbol;
+	if (toupper(symbol) >= 'A' && toupper(symbol) <= 'Z')
+		if (toupper(symbol) == 'A' || toupper(symbol) == 'E' || toupper(symbol) == 'I' || toupper(symbol) == 'O' || toupper(symbol) == 'U' || toupper(symbol) == 'Y')
+			std::cout << "vowel";
+		else
+			std::cout << "consonant";
+	else
+		std::cout << "Not letter";
+	return 0;
+}
